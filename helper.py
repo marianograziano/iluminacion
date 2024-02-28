cdb191e69eb42c19df31e0b19c832aeee765af35
Mariano Graziano <mariano.graziano@gmail.com>
2024-02-28 09:45:49 -0300

    IKhana class done

--- helper.py
+++ helper.py
@@ -28,3 +28,32 @@ def extraer_fecha(texto):
     except ValueError:
         # Manejo de la excepción si ocurre un error en la conversión de datos
         return "No se puede extraer la fecha debido a un error en el formato."
+    
+
+def imprimir_tipo(cadena):
+    # Patrón para verificar si comienza con 'U' seguido de un número
+    patron_unipolar = re.compile(r'^U\d')
+    # Patrón para verificar si comienza con 'UB' seguido de un número
+    patron_unipolar_bobina = re.compile(r'^UB\d')
+    patron_paralelo = re.compile(r'^P\d')
+    patron_bipolar_rojo_negro= re.compile(r'^B\d')
+    patron_tipo_taller = re.compile(r'^T\d')
+    patron_tipo_taller_bobina = re.compile(r'^TB\d')
+    patron_tipo_subterraneo = re.compile(r'^S\d')
+
+    if patron_unipolar_bobina.match(cadena):
+        return ("Unipolar bobina")
+    elif patron_unipolar.match(cadena):
+        return ("Unipolar")
+    elif patron_paralelo.match(cadena):  # Added missing reference to "patron_paralelo"
+        return ("Paralelo")  # Fixed return value
+    elif patron_bipolar_rojo_negro.match(cadena):  # Added missing reference to "patron_bipolar_rojo_negro"
+        return ("Bipolar rojo negro")  # Fixed return value
+    elif patron_tipo_taller.match(cadena):
+        return ("Tipo Taller Envainado")
+    elif patron_tipo_taller_bobina.match(cadena):
+        return ("Tipo taller Envainado Bobina")
+    elif patron_tipo_subterraneo.match(cadena):
+        return ("Subterraneo Flexible")
+    else:
+        return("No coincide")
